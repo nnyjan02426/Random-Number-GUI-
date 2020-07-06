294564949a1bfc8a83943fe7794d8af6468c8b29
NY Jan <nnyjan02426@gmail.com>
2020-07-06 11:31:44 +0800

    sort functions

--- GUI.py
+++ GUI.py
@@ -10,28 +10,44 @@ window.configure(background='white')
 
 start_frame = tk.Frame(window)
 start_frame.pack(side=tk.TOP)
-tk.Label(start_frame, text = '起始數字').pack(side=tk.LEFT)
+tk.Label(start_frame, text='起始數字').pack(side=tk.LEFT)
 start_num = tk.Entry(start_frame)
 start_num.pack(side=tk.LEFT)
 
 end_frame = tk.Frame(window)
 end_frame.pack(side=tk.TOP)
-tk.Label(end_frame, text = '結束數字').pack(side=tk.LEFT)
+tk.Label(end_frame, text='結束數字').pack(side=tk.LEFT)
 end_num = tk.Entry(end_frame)
 end_num.pack(side=tk.LEFT)
 tk.Label(window).pack()
 
-
-rand_output = tk.Label(window, font=('Times',50))
+rand_output = tk.Label(window, font=('Times', 50))
 rand_output.pack()
 
-repeat_lst = []
-lst_num = 1
-clear_all = False
 
-class commands():
+def if_input_err(num1, num2):
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+
+        if num1 == num2:
+            tk.messagebox.showerror('ERROR', '請輸入相異的數字')
+            return True
+        if num1 > num2:
+            tk.messagebox.showerror('ERROR', '起始數字 需比 結束數字 大')
+            return True
+
+    except ValueError:
+        tk.messagebox.showerror('ERROR', '請輸入正整數')
+        return True
+
+    return False
+
+
+class Commands:
     def __init__(self):
         self.repeat_bool = False
+        self.repeat_lst = []
 
     def repeat(self):
         if variable1.get() == 1:
@@ -40,56 +56,50 @@ class commands():
             self.repeat_bool = False
 
     def clear_hist(self):
-        global repeat_lst
-        repeat_lst = []
+        self.repeat_lst = []
         rand_output.configure(text=' ')
 
-    def rand_command(self):
-        global repeat_lst
+    def main(self):
+        go = True
         num1 = start_num.get()
         num2 = end_num.get()
-        try:
-            num1_int = int(num1)
-            num2_int = int(num2)
-            if num1_int == num2_int:
-                tk.messagebox.showerror('ERROR', '請輸入相異的數字')
-            elif num1_int > num2_int:
-                tk.messagebox.showerror('ERROR', '起始數字 需比 結束數字 大')
-            else:
-                if len(repeat_lst) == len(range(num1_int, num2_int+1)):
-                    if tk.messagebox.askyesno(' ', '所有數字已輪過一遍，是否重輪一遍？') == True:
-                        rand_output.configure(text=' ')
-                        repeat_lst = []
-                    else:
-                        window.destroy()
+        if not if_input_err(num1, num2):
+            num1 = int(num1)
+            num2 = int(num2)
+            if len(self.repeat_lst) == len(range(num1, num2 + 1)):
+                if tk.messagebox.askyesno(' ', '所有數字已輪過一遍，是否重輪一遍？'):
+                    rand_output.configure(text=' ')
+                    self.repeat_lst = []
+                    go = False
                 else:
-                    rand_num = random.randint(num1_int, num2_int)
-                    if self.repeat_bool == False:
-                        while rand_num in repeat_lst:
-                            rand_num = random.randint(num1_int, num2_int)
-                        else:
-                            rand_output.configure(text=str(rand_num))
-                            self.lst_get = True
-                            repeat_lst.append(rand_num)
-                            self.lst_get = False
+                    go = False
+                    window.destroy()
+            else:
+                rand_num = random.randint(num1, num2)
+
+            if go:
+                if not self.repeat_bool:
+                    while rand_num in self.repeat_lst:
+                        rand_num = random.randint(num1, num2)
                     else:
                         rand_output.configure(text=str(rand_num))
-        except ValueError:
-            tk.messagebox.showerror('ERROR', '請輸入正整數')
+                        self.repeat_lst.append(rand_num)
+                else:
+                    rand_output.configure(text=str(rand_num))
 
 
 tk.Label(window).pack()
-comm = commands()
+command = Commands()
 
-rand_button = ttk.Button(window, text='RUN', command=comm.rand_command)
+rand_button = ttk.Button(window, text='RUN', command=command.main)
 rand_button.pack()
 
 check_frame = tk.Frame(window)
 check_frame.pack(side=tk.BOTTOM)
 variable1 = tk.IntVar()
-repeat_check = tk.Checkbutton(check_frame, text='重複', variable=variable1, onvalue=1, offvalue=0, command=comm.repeat)
+repeat_check = tk.Checkbutton(check_frame, text='重複', variable=variable1, onvalue=1, offvalue=0, command=command.repeat)
 repeat_check.pack(side=tk.LEFT)
-clear_hist_check = ttk.Button(check_frame, text='清除歷史', command=comm.clear_hist)
+clear_hist_check = ttk.Button(check_frame, text='清除歷史', command=command.clear_hist)
 clear_hist_check.pack(side=tk.LEFT)
 
 window.mainloop()
